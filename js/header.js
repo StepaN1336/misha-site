1d1f846ee2eeaa85750d2364f6d8eacabcb61875
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2024-01-12 01:34:23 +0000

    add index.html page v0.1.0

--- js/header.js
+++ js/header.js
@@ -1,9 +1,18 @@
-$(function () {
-    $(window).on('scroll', function () {
-        if ( $(window).scrollTop() > 10 ) {
-            $('.navbar').addClass('active');
-        } else {
-            $('.navbar').removeClass('active');
-        }
-    });
-});
+const burgerIcon = document.getElementById("burgerIcon");
+const modal = document.getElementById("hamb_modal");
+
+function closeModal() {
+  if (window.innerWidth > 990) {
+    modal.classList.remove("active");
+    burgerIcon.classList.remove("active");
+  }
+}
+
+function toggleModal() {
+  modal.classList.toggle("active");
+  burgerIcon.classList.toggle("active");
+}
+
+burgerIcon.addEventListener("click", toggleModal);
+
+window.addEventListener("resize", closeModal);
