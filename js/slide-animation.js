30bbfb779e4d34d91cb28f7139b65fde0709fc93
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2024-01-13 17:27:13 +0000

    remove logs, comment unobserve to see if this was the problem

--- js/slide-animation.js
+++ js/slide-animation.js
@@ -5,12 +5,10 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('fade-in-animation');
-            observer.unobserve(entry.target);
-            console.log('end');
+            //observer.unobserve(entry.target);
         }
     });
 });
 
 observer.observe(newsAnimationContainer);
 observer.observe(featuresAnimationContainer);
-console.log('end');
