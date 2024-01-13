55461c1ef11013585facec7a5f5cfd4ff60e41c9
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2024-01-13 17:10:47 +0000

    add DOMContentLoaded to fix null in JS animation code

--- js/slide-animation.js
+++ js/slide-animation.js
@@ -1,15 +1,15 @@
-const newsAnimationContainer = document.querySelector('.news__wrapper');
-const featuresAnimationContainer = document.querySelector('.features_wrapper');
-const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-        // If the element is visible
-        if (entry.isIntersecting) {
-            // Add the animation class
-            entry.target.classList.add('fade-in-animation');
-        }
+document.addEventListener('DOMContentLoaded', function () {
+    const newsAnimationContainer = document.querySelector('.news__wrapper');
+    const featuresAnimationContainer = document.querySelector('.features_wrapper');
+    
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-animation');
+            }
+        });
     });
-});
 
-observer.observe(newsAnimationContainer);
-observer.observe(featuresAnimationContainer);
+    observer.observe(newsAnimationContainer);
+    observer.observe(featuresAnimationContainer);
+});
