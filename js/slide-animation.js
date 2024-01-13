9c64272ac889cf2423359ea4681ef74a9574e0c8
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2024-01-13 17:24:35 +0000

    add logs to see animation bugs

--- js/slide-animation.js
+++ js/slide-animation.js
@@ -1,15 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const newsAnimationContainer = document.querySelector('.news__wrapper');
-    const featuresAnimationContainer = document.querySelector('.features__wrapper');
-    
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('fade-in-animation');
-            }
-        });
-    });
+const newsAnimationContainer = document.querySelector('.news__wrapper');
+const featuresAnimationContainer = document.querySelector('.features__wrapper');
 
-    observer.observe(newsAnimationContainer);
-    observer.observe(featuresAnimationContainer);
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-in-animation');
+            observer.unobserve(entry.target);
+            console.log('end');
+        }
+    });
 });
+
+observer.observe(newsAnimationContainer);
+observer.observe(featuresAnimationContainer);
+console.log('end');
