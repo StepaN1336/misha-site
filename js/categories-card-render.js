01c3f26a56410316e35bbc25c655cd9b29a341a1
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2024-01-28 03:12:38 +0000

    add production page, add categories, add render categories from json file

---
+++ js/categories-card-render.js
@@ -0,0 +1,30 @@
+const categoriesContainer = document.querySelector('.production__categories-cards');
+let title = null;
+let image = null;
+let categoriesCount = 0;
+
+fetch('../json/categories.json')
+    .then(response => response.json())
+    .then(jsonContent => {
+        for (const categoryKey in jsonContent) {
+            if (jsonContent.hasOwnProperty(categoryKey)) {
+                const category = jsonContent[categoryKey];
+                const title = category.title;
+                const image = category.image;
+                
+                const categoryCard = document.createElement('div');
+                categoryCard.classList.add('production__category-card');
+
+                categoryCard.style.backgroundImage = `url(${image})`;
+                const titleElement = document.createElement('h2');
+                titleElement.textContent = title;
+                titleElement.classList.add('production__category-title');
+                categoryCard.appendChild(titleElement);
+
+                categoriesContainer.appendChild(categoryCard);
+            }
+        }
+
+    })
+    .catch(err => console.error('Помилка читання файлу:', err));
+
---
+++ js/production.js
@@ -0,0 +1,4 @@
+document.addEventListener('partialsLoaded', () => {
+    import('./header.js');
+    import('./categories-card-render.js');
+});
