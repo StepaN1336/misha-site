1fe9ee1d04165c4b9cf9c6b73c86d483a32c8776
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2024-02-04 03:35:21 +0000

    header final 0.1.0, upload photos

--- js/header.js
+++ js/header.js
@@ -1,18 +1,4 @@
-const burgerIcon = document.getElementById("burgerIcon");
-const modal = document.getElementById("hamb_modal");
-
-function closeModal() {
-  if (window.innerWidth > 990) {
-    modal.classList.remove("active");
-    burgerIcon.classList.remove("active");
-  }
-}
-
-function toggleModal() {
-  modal.classList.toggle("active");
-  burgerIcon.classList.toggle("active");
-}
-
-burgerIcon.addEventListener("click", toggleModal);
-
-window.addEventListener("resize", closeModal);
+document.querySelector('.header__burger').addEventListener('click', () => {
+  this.classList.toggle('active');
+  document.querySelector('.header__navigation-menu').classList.toggle('open');
+});
