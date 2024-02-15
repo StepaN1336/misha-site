ecc559f57dc0aafe73ef1ca5b20acff305a91fd7
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2024-02-15 15:54:58 +0000

    add burger menu

--- js/header.js
+++ js/header.js
@@ -1,4 +1,9 @@
-document.querySelector('.header__burger').addEventListener('click', () => {
-  this.classList.toggle('active');
-  document.querySelector('.header__navigation-menu').classList.toggle('open');
+const headerBurger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__navigation-links');
+const body = document.body;
+
+headerBurger.addEventListener('click', () => {
+  headerBurger.classList.toggle('active');
+  headerMenu.classList.toggle('open');
+  body.classList.toggle('lock')
 });
