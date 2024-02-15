0603eba390ec916e787103c04122d34f1389d92e
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2024-02-15 14:29:38 +0000

    add vacancies css v0.1.0

--- js/vacancies/vacancies.js
+++ js/vacancies/vacancies.js
@@ -14,13 +14,17 @@ function cardRender(json) {
                     const vacancyCard = document.createElement('div');
                     vacancyCard.classList.add('vacancy__card');
                     const vacancyTitle = document.createElement('h2');
+                    vacancyTitle.classList.add('vacancy__card-title');
                     vacancyTitle.textContent = name;
                     const vacancyDescription = document.createElement('p');
+                    vacancyDescription.classList.add('vacancy__card-description');
                     vacancyDescription.textContent = description;
                     const requirementsList = document.createElement('ul');
+                    requirementsList.classList.add('vacancy__card-list');
 
                     for (const req of requirements) {
                         const reqListItem = document.createElement('li');
+                        reqListItem.classList.add('vacancy__card-list-item');
                         reqListItem.textContent = req;
                         requirementsList.appendChild(reqListItem);
                     }
