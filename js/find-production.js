86cc2c7e73434b9ad1799c1ebda4813d516764e8
Stepan Syhil <114647738+StepaN1336@users.noreply.github.com>
2024-08-30 19:11:57 +0000

    add find-product responsive

--- js/find-production.js
+++ js/find-production.js
@@ -2,15 +2,15 @@ const examples = [
     {
         id: 1,
         title: 'Точно знайдеш щось для себе!',
-        description: 'Різноманітні ковбаски, сосиски, делікатеси, копчені вироби та навіть тваринний корм та випічка.',
-        image: 'images/копчені-вироби/test.jpg',
+        description: 'Різноманітні ковбаски, сосиски, делікатеси, копчені вироби та навіть тваринний корм та випічка. Ми пропонуємо кожному клієнту вибір, зробивши який, кожен клінєт залишиться задоволеним. Кожна позиція вирізняється свіжістю та якістю, адже ми піклуємося про безпеку кожного клієнта та репутацію торгової марки',
+        image: 'images/test1.jpg',
         alt: 'Приклад вироба'
     },
     {
         id: 2,
         title: 'Смачність на кожен день!',
-        description: 'Для вашої зручності все це ми зібрали для вас тут, тому перш ніж завітати до нас в гості, можете підшукати апетитну ковбаску, що вам сподобається',
-        image: 'images/копчені-вироби/test.jpg',
+        description: 'Для вашої зручності все це ми зібрали для вас тут, тому перш ніж завітати до нас в гості, можете підшукати апетитну ковбаску, що вам сподобається. Після вибору з нашого каталогу, ви легко можете знайти найближчий до вас магазин також саме тут, на нашому сайті.',
+        image: 'images/test3.jpg',
         alt: 'Приклад вироба'
     }
 ]
@@ -21,6 +21,7 @@ function renderExamples(examples) {
     const examplesContainer = document.querySelector('.find-production__example');
     for(const example of examples) {
         const exampleCard = document.createElement('div');
+        const textContentContainer = document.createElement('div');
         const image = document.createElement('img');
         const title = document.createElement('h3');
         const description = document.createElement('p');
@@ -35,9 +36,12 @@ function renderExamples(examples) {
         description.textContent = example.description;
         description.classList.add('example__description');
 
+        textContentContainer.appendChild(title);
+        textContentContainer.appendChild(description);
+        textContentContainer.classList.add('example__text-content-container');
+
         exampleCard.appendChild(image);
-        exampleCard.appendChild(title);
-        exampleCard.appendChild(description);
+        exampleCard.appendChild(textContentContainer);
         exampleCard.classList.add('example__card');
 
         examplesContainer.appendChild(exampleCard);
