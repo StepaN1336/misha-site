7bbfccec713955f2f7a63261880aad4472eb8f4f
Степан <ssigilstopa@gmail.com>
2024-12-28 04:02:42 +0200

    Compress images, convert into webp, fix categories render

--- js/card-render/categories-card-render.js
+++ js/card-render/categories-card-render.js
@@ -1,14 +1,34 @@
+const categoriesContainer = document.querySelector('.production__categories-cards');
+
 fetch('json/categories.json')
     .then(response => response.json())
     .then(jsonContent => {
-        categoriesContainer.innerHTML = Object.values(jsonContent)
-            .map(({ title, image, htmlPage: htmlUrl }) => `
-                <a href="${htmlUrl}" class="production__card">
-                    <div class="production__card-img-wrapper">
-                        <img src="${image}" loading="lazy" alt="${title}" class="production__card-img">
-                    </div>
-                    <h2 class="production__card-title">${title}</h2>
-                </a>
-            `).join('');
+        for (const categoryKey in jsonContent) {
+            if (jsonContent.hasOwnProperty(categoryKey)) {
+                const category = jsonContent[categoryKey];
+                const title = category.title;
+                const image = category.image;
+                const htmlUrl = category.htmlPage;
+
+                const link = document.createElement('a');
+                link.href = htmlUrl;
+                link.classList.add('production__card-link')
+
+                const categoryCard = document.createElement('div');
+                categoryCard.classList.add('production__category-card');
+                categoryCard.style.backgroundImage = `url(${image})`;
+                categoryCard.style.backgroundPosition = 'center center';
+                
+                const titleElement = document.createElement('h2');
+                titleElement.textContent = title;
+                titleElement.classList.add('production__category-title');
+                categoryCard.appendChild(titleElement);
+
+                link.appendChild(categoryCard);
+
+                categoriesContainer.appendChild(link);
+            }
+        }
+
     })
     .catch(err => console.error('Помилка читання файлу:', err));
--- js/card-render/production-card-render.js
+++ js/card-render/production-card-render.js
@@ -1,36 +1,14 @@
-export function cardRender(json, productionTitle) {
-
-    const bakingContainer = document.querySelector('.production__cards-container');
-    const productTitle = document.querySelector('.production__title');
-    productTitle.textContent = productionTitle;
-
-    fetch(json)
-        .then(response => response.json())
-        .then(jsonContent => {
-            for (const categoryKey in jsonContent) {
-                if (jsonContent.hasOwnProperty(categoryKey)) {
-                    const category = jsonContent[categoryKey];
-                    const title = category.title;
-                    const image = category.image;
-
-                    const categoryCard = document.createElement('div');
-                    categoryCard.classList.add('production__card');
-
-                    const categoryImage = document.createElement('img');
-                    categoryImage.setAttribute('src', image);
-                    categoryImage.classList.add('production__card-image');
-
-                    const categoryTitle = document.createElement('h2');
-                    categoryTitle.textContent = title;
-                    categoryTitle.classList.add('production__card-title');
-
-                    categoryCard.appendChild(categoryImage);
-                    categoryCard.appendChild(categoryTitle);
-
-                    bakingContainer.appendChild(categoryCard);
-                }
-            }
-
-        })
-        .catch(err => console.error('Помилка читання файлу:', err));
-}
+fetch('json/categories.json')
+    .then(response => response.json())
+    .then(jsonContent => {
+        categoriesContainer.innerHTML = Object.values(jsonContent)
+            .map(({ title, image, htmlPage: htmlUrl }) => `
+                <a href="${htmlUrl}" class="production__card">
+                    <div class="production__card-img-wrapper">
+                        <img src="${image}" loading="lazy" alt="${title}" class="production__card-img">
+                    </div>
+                    <h2 class="production__card-title">${title}</h2>
+                </a>
+            `).join('');
+    })
+    .catch(err => console.error('Помилка читання файлу:', err));
