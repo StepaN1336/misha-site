393fdf88468b40fac1280a94b32799a9fa86fcee
Степан <ssigilstopa@gmail.com>
2024-12-28 05:14:45 +0200

    Renew addresses, fix index images

--- js/card-render/myasni-delikatesy.js
+++ js/card-render/myasni-delikatesy.js
@@ -1,5 +1,5 @@
 import { cardRender } from './production-card-render.js';
 document.addEventListener('partialsLoaded', () => {
     import('./header.js');
-    cardRender('../json/card-render/myasni-delikatesy.json', "М'ясні делікатеси");
+    cardRender('../json/card-render/myasni-delikatesy.json', "М'ясні Делікатеси");
 });
